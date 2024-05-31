91141b10500fb666e544e52f9e0294ebdfced957
Chris <sdecuir12@gmail.com>
2024-05-31 17:09:14 -0500

    html structure

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-const menuItems = document.querySelectorAll('p');
-const burgersvg = document.querySelector('.burgersvg');
+const docEl = (function() {
+  const outer = document.querySelector('.outer');
+  const inner = document.querySelector('.inner');
+  const leftArrow = document.querySelector('#left');
+  const pics = document.querySelectorAll('.pic');
+  const pic1 = document.querySelector('#pic1');
+  const pic2 = document.querySelector('#pic2');
+  const pic3 = document.querySelector('#pic3');
+  const rightArrow = document.querySelector('#right');
+  const dot1 = document.querySelector('#dot1');
+  const dot2 = document.querySelector('#dot2');
+  const dot3 = document.querySelector('#dot3');
 
-burgersvg.addEventListener("click", (event) =>   {
-    for (const item of menuItems) {
-    item.classList.toggle('invisible')
-}
-})
+  return {
+    outer,
+    inner,
+    leftArrow,
+    pics,
+    pic1,
+    pic2,
+    pic3,
+    rightArrow,
+    dot1,
+    dot2,
+    dot3
+  };
+});
+
+  function appendPicture  ()
