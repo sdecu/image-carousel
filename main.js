ff9719aa3eaf908b425f162028e7982eabbf5a67
Chris <sdecuir12@gmail.com>
2024-06-01 17:36:01 -0500

    add function to change picture when client clicks on dot

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const docEl = (function() {
   const pic2 = document.querySelector('#pic2');
   const pic3 = document.querySelector('#pic3');
   const rightArrow = document.querySelector('#right');
+  const dotsDiv = document.querySelector('.dots-div')
+  const dots = document.querySelectorAll('.dots')
   const dot1 = document.querySelector('#dot1');
   const dot2 = document.querySelector('#dot2');
   const dot3 = document.querySelector('#dot3');
@@ -20,10 +22,27 @@ const docEl = (function() {
     pic2,
     pic3,
     rightArrow,
+    dotsDiv,
+    dots,
     dot1,
     dot2,
     dot3
   };
-});
+})();
 
-  function appendPicture  ()
+docEl.outer
+
+
+docEl.dot1.onclick = (event) => { appendPictureWithDot(docEl.dot1, docEl.pic1)};
+docEl.dot2.onclick = (event) => { appendPictureWithDot(docEl.dot2, docEl.pic2)};
+docEl.dot3.onclick = (event) => { appendPictureWithDot(docEl.dot3, docEl.pic3)};
+
+  function appendPictureWithDot  (dot, pic) {
+    pic.remove();
+    docEl.inner.prepend(pic);
+    
+    for (element of docEl.dots) {
+      element.style.fill = 'black';
+    };
+    dot.style.fill = 'white';
+  }
